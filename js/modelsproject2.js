0693aef5ea6b5744110aa22546fe4e83c62b6c3b
SzilviaDeahl <szilvia.deahl@yahoo.com>
2015-10-21 15:53:40 -0600

    project on models exercise finished second time

---
+++ js/modelsproject2.js
@@ -0,0 +1,39 @@
+var Vehicle = Backbone.Model.extend({
+  validate: function (attrs) {
+    if(!attrs.registrationNumber)
+      return 'Registration number is required';
+  },
+
+  urlRoot: '/api/vehicles',
+
+  start: function () {
+    console.log("Vehicle started");
+  }
+});
+
+var Car = Vehicle.extend({
+  start: function () {
+    console.log('Car with registration number ' + this.get('registrationNumber') + ' started');
+  }
+});
+
+var car =  new Car({
+  registrationNumber: 'XLI228',
+  color: 'Black'
+});
+car.start();
+
+car.unset('registrationNumber');
+
+if (!car.isValid())
+  console.log(car.validationError);
+
+car.start();
+
+car.set('registrationNumber', 'abc123');
+
+if (!car.isValid())
+  console.log(car.validationError);
+  else {
+    console.log(car.attributes.color, car.attributes.registrationNumber);
+  }
