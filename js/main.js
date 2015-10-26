43affbefbe0785d14b2fa0c3813f9f218b443258
SzilviaDeahl <szilvia.deahl@yahoo.com>
2015-10-26 11:17:46 -0600

    handling collection events in backbone exercise finished

--- js/main.js
+++ js/main.js
@@ -238,37 +238,118 @@
  // ***************** Creating Views end *******************
 
 // **************** Handling DOM Events *******************
+//
+// // var Backbone = require('backbone'); // example: backbone is imported
+// // // Add this!
+// // if (window.__backboneAgent) {
+// //   window.__backboneAgent.handleBackbone(Backbone);
+// // };
+//
+// var Song = Backbone.Model.extend();
+//
+// var SongView = Backbone.View.extend({
+//   events: {
+//     'click': 'onClick',
+//     'click .bookmark': 'onClickBookmark',
+//   },
+//
+//   onClick: function () {
+//     console.log('Listen Clicked');
+//   },
+//   onClickBookmark: function (e) {
+//     e.stopPropagation();
+//     console.log('Bookmark Clicked');
+//   },
+//
+//   render: function () {
+//     this.$el.html(this.model.get('title') + '<button>Listen</button> <button class="bookmark">Bookmark</button>');
+//
+//     return this;
+//   }
+// });
+//
+// var song = new Song({title: 'Blue in Green'});
+//
+// var songView = new SongView({ el: '#container', model: song});
+// songView.render();
+//
+// ****************** Handling DOM events end ********************
 
-// var Backbone = require('backbone'); // example: backbone is imported
-// // Add this!
-// if (window.__backboneAgent) {
-//   window.__backboneAgent.handleBackbone(Backbone);
-// };
+// ***************** Handling Model Events **********************
+//
+// var Song = Backbone.Model.extend({
+//   defaults: {
+//     listeners: 0
+//   }
+// });
+//
+// var SongView = Backbone.View.extend({
+//   initialize: function () {
+//     this.model.on('change', this.render, this);
+//   },
+//   render: function () {
+//     this.$el.html(this.model.get('title') + ' - Listeners: ' + this.model.get('listeners'));
+//
+//     return this;
+//   }
+// });
+//
+// var song =  new Song({title: 'Blue in Green'});
+//
+// var songView = new SongView({ el: '#container', model: song});
+// songView.render();
+//
+// ***************** Handling Model Events end *********************
 
-var Song = Backbone.Model.extend();
-
-var SongView = Backbone.View.extend({
-  events: {
-    'click': 'onClick',
-    'click .bookmark': 'onClickBookmark',
-  },
-
-  onClick: function () {
-    console.log('Listen Clicked');
-  },
-  onClickBookmark: function (e) {
-    e.stopPropagation();
-    console.log('Bookmark Clicked');
-  },
-
-  render: function () {
-    this.$el.html(this.model.get('title') + '<button>Listen</button> <button class="bookmark">Bookmark</button>');
-
-    return this;
-  }
-});
-
-var song = new Song({title: 'Blue in Green'});
-
-var songView = new SongView({ el: '#container', model: song});
-songView.render();
+// **************** Handling Collection Events *******************
+//
+// var Song = Backbone.Model.extend();
+//
+// var Songs = Backbone.Collection.extend({
+//   model: Song
+// });
+//
+// var SongView = Backbone.View.extend({
+//   tagName: 'li',
+//
+//   render: function () {
+//     this.$el.html(this.model.get('title'));
+//     this.$el.attr('id', this.model.id);
+//     return this;
+//   }
+// });
+//
+// var SongsView = Backbone.View.extend({
+//   tagName: 'ul',
+//
+//   initialize: function () {
+//     this.model.on('add', this.onSongAdded, this);
+//     this.model.on('remove', this.onSongRemoved, this);
+//   },
+//   onSongAdded: function (song) {
+//     var songView = new SongView({ model: song });
+//     this.$el.append(songView.render().$el);
+//   },
+//   onSongRemoved: function (song) {
+//     this.$('li#' + song.id).remove();
+//   },
+//
+//   render: function () {
+//     var self = this;
+//     this.model.each(function (song) {
+//       var songView = new SongView({ model: song });
+//       self.$el.append(songView.render().$el);
+//     });
+//   }
+// });
+//
+// var songs = new Songs([
+//   new Song({id: 1, title: 'Blue in Green'}),
+//   new Song({id: 2, title: 'So what'}),
+//   new Song({id: 3, title: 'All Blues'})
+// ]);
+//
+// var songsView = new SongsView({ el: '#songs', model: songs});
+// songsView.render();
+//
+// *************** Handling Collection EVents end ******************
