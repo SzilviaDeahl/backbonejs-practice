04b2b60d4bd41d3f07eb3cefa8357c3391440788
Szilvia Deahl <sdeahl@talentreef.com>
2015-11-13 11:45:27 -0700

    fixed folder naming

--- js/vehicles.js
+++ js/vehicles.js
@@ -1,59 +1,59 @@
-// var Vehicle = Backbone.Model.extend();
-//
-// var Vehicles = Backbone.Collection.extend({
-//   model: Vehicle
-// });
-//
-// var VehicleView = Backbone.View.extend({
-//   tagName: 'li',
-//
-//   events: {
-//     'click .delete': 'delete',
-//   },
-//
-//   delete: function (vehicle) {
-//     this.remove();
-//   },
-//
-//   render: function () {
-//     this.$el.html(this.model.get('registrationNumber') + '<button class="delete">Delete</button>');
-//     return this;
-//   }
-// });
-//
-// var VehiclesView = Backbone.View.extend({
-//   tagName: 'ul',
-//   render: function () {
-//     var self = this;
-//     this.model.each(function (vehicle) {
-//       var vehicleView = new VehicleView({ model: vehicle});
-//       self.$el.append(vehicleView.render().$el);
-//     });
-//   }
-// });
-// // Aggregation
-// var NewVehicleView = Backbone.View.extend({
-//
-//   events: {
-//     'click .add': 'add'
-//   },
-//
-//   add: function () {
-//     this.collection.add();
-//   },
-//
-//   render: function () {
-//     this.$el.append('<input id="foobar" type="text"></input>' + '<button class="add">Add</button>')
-//   }
-// });
-//
-// var newVehiclesView = new NewVehicleView({ el: '#foobar', model: vehicles});
-// newVehiclesView.render();
-//
-// var vehicles = new Vehicles([
-//   new Vehicle({name: 'car1', registrationNumber: 'XLI887'}),
-//   new Vehicle({name: 'car2', registrationNumber: 'ZNP123'})
-// ]);
-//
-// var vehiclesView = new VehiclesView({ el: '#vehicles', model: vehicles });
-// vehiclesView.render();
+var Vehicle = Backbone.Model.extend();
+
+var Vehicles = Backbone.Collection.extend({
+  model: Vehicle
+});
+
+var VehicleView = Backbone.View.extend({
+  tagName: 'li',
+
+  events: {
+    'click .delete': 'delete',
+  },
+
+  delete: function (vehicle) {
+    this.remove();
+  },
+
+  render: function () {
+    this.$el.html(this.model.get('registrationNumber') + '<button class="delete" style="color:blue; margin: 5px;">Delete</button>');
+    return this;
+  }
+});
+
+var VehiclesView = Backbone.View.extend({
+  tagName: 'ul',
+  render: function () {
+    var self = this;
+    this.model.each(function (vehicle) {
+      var vehicleView = new VehicleView({ model: vehicle});
+      self.$el.append(vehicleView.render().$el);
+    });
+  }
+});
+// Aggregation
+var NewVehicleView = Backbone.View.extend({
+
+  events: {
+    'click .add': 'add'
+  },
+
+  add: function () {
+    this.collection.add();
+  },
+
+  render: function () {
+    this.$el.append('<input id="foobar" type="text"></input>' + '<button class="add">Add</button>')
+  }
+});
+
+var newVehiclesView = new NewVehicleView({ el: '#foobar', model: vehicles});
+newVehiclesView.render();
+
+var vehicles = new Vehicles([
+  new Vehicle({name: 'car1', registrationNumber: 'XLI887'}),
+  new Vehicle({name: 'car2', registrationNumber: 'ZNP123'})
+]);
+
+var vehiclesView = new VehiclesView({ el: '#vehicles', model: vehicles });
+vehiclesView.render();
